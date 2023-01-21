4afdb501c9055d2871daa77d7b8dd63316bd1414
Mahady Al Hady <mahady426@gmail.com>
2023-01-22 05:08:03 +0600

    Update 2023_01_21_205326_create_todos_table.php

--- database/migrations/2023_01_21_205326_create_todos_table.php
+++ database/migrations/2023_01_21_205326_create_todos_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('isDone')->default(false);
             $table->timestamps();
         });
     }
