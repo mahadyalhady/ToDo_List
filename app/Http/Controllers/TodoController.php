5eff24bae67cf260d348af12e4614ab829320c1b
Mahady Al Hady <mahady426@gmail.com>
2023-01-22 05:07:51 +0600

    Update TodoController.php

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index(){
+        $todos=Todo::all();
+        return view('welcome', ['todos'=>$todos]);
+    }
+
+    public function store(){
+        $attributes=request()->validate(
+            ['title'=>'required',
+            'description'=>'nullable']
+        );
+
+        Todo::created($attributes);
+        
+        return redirect('/');
+    }
 }
